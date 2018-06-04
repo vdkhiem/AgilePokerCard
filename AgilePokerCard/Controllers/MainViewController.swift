6b8f2cf6fb2f6ed9545c3da9dba0ad36cc396f93
Vo Duy Khiem <dkvo@trans-mbp.hub>
2018-06-04 13:23:29 +1200

    Added t-shirt estimation

--- AgilePokerCard/Controllers/MainViewController.swift
+++ AgilePokerCard/Controllers/MainViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class MainViewController: UIViewController {
     var selectedCard = UIButton()
+    var estimationTypeselectedIndex: Int?
+    
+    @IBOutlet weak var pokerView: UIView!
+    @IBOutlet weak var estimationTupeSegmentControl: UISegmentedControl!
+    @IBOutlet weak var tshirtView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //UIApplication.shared.statusBarView? = UIColor.white
+        if let selectIndex = estimationTypeselectedIndex {
+            estimationTupeSegmentControl.selectedSegmentIndex = selectIndex
+        }
+        displayEstimationType()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func estimationTypeSegementValueChanged(_ sender: UISegmentedControl) {
+        estimationTypeselectedIndex = estimationTupeSegmentControl.selectedSegmentIndex
+        displayEstimationType()
+        
     }
     
     @IBAction func buttonPress(_ sender: UIButton) {
@@ -33,6 +48,19 @@ class MainViewController: UIViewController {
             desViewController.textPassedOver = selectedCard.titleLabel!.text
             
         }
+    }
+    
+    func displayEstimationType() {
+        if estimationTupeSegmentControl.selectedSegmentIndex == 0 {
+            showPoker(isVisible: true)
+        } else {
+            showPoker(isVisible: false)
+        }
+    }
+    
+    func showPoker(isVisible: Bool) {
+        pokerView.isHidden = !isVisible
+        tshirtView.isHidden = isVisible
     }
     
 }
